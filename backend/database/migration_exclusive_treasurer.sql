cb4b31ca8acab3e32525d6609709106a8180c9c5
Bavikaran Yogeswaran <bavikaranyogeswaran@gmail.com>
2026-03-14 16:18:26 +0530

    removed unnecessary files

--- backend/database/migration_exclusive_treasurer.sql
+++
@@ -1,5 +0,0 @@
--- Remove the old non-exclusive unique key
-ALTER TABLE staff_property_assignments DROP INDEX unique_assignment;
-
--- Add the new exclusive unique key
-ALTER TABLE staff_property_assignments ADD UNIQUE KEY unique_property (property_id);
